fc4a6fc0690d66944dffbdbc07d152c39ea40b44
Oliver Cordingley <thatkidwhocodes@gmail.com>
2018-02-27 20:52:55 +0000

    main container and nav

---
+++ src/components/Container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navigation/>
+    <Vote name="hsdfjkl" image="www.image.com" description="fdsa"/>
+  </div>
+</template>
+
+<script>
+  import Navigation from '@/components/Navigation'
+  import Vote from '@/components/Vote'
+
+  export default {
+    name: 'container',
+    template: '<Container/>',
+    components: {
+      Navigation,
+      Vote
+    }
+  }
+</script>
+
+<style scoped>
+  div{
+    margin: 20px auto;
+    width: 60%;
+    min-height: 500px;
+    background-color: #eee;
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li>All</li>
+      <li>List</li>
+      <li>Submit</li>
+    </ul><hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  template: '<Navigation/>'
+}
+</script>
+
+<style scoped>
+  ul {
+
+    height: 25px;
+    font-size: 20px;
+    font-weight:bold;
+    text-transform:uppercase;
+    list-style-type: none;
+    position: relative;
+  }
+  li{
+    height: 25px;
+    top:30%;
+    bottom: 40%;
+    display: inline-block;
+    position: relative;
+    margin-right: 15px;
+    left:0;
+    right:0;
+  }
+</style>
