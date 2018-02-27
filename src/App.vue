8cab919492b7edc702a796beb4a553fde07b4ab4
Oliver Cordingley <thatkidwhocodes@gmail.com>
2018-02-27 20:52:22 +0000

    Setting up

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Container/>
   </div>
 </template>
 
 <script>
+import Container from '@/components/Container'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Container
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Impact', 'Arial Black', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
   margin-top: 60px;
 }
 </style>
