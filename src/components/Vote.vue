00581f684aa15016bf6dd9bad52f652fc22226a8
Oliver Cordingley <thatkidwhocodes@gmail.com>
2018-02-27 20:53:20 +0000

    Started on Vote page
    TODO:    MAKE IT WORK

---
+++ src/components/Vote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ name }} hjkfdah</h1>
+    <img :src="image" :alt="name"/><br>
+    <p>
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vote',
+  props: {
+    name: {
+      default: 'No memes to vote.'
+    },
+    image: {
+      default: 'http://via.placeholder.com/350?text=Placeholder+Meme'
+    },
+    description: {
+      default: 'lo dfhasjl,gvkfh sjkadfgdfsg dfjsg dgjdsfjkfd gfjodgs kfdsg hfjkdsjk;f dyi kfhd d fhdjkslagh dlgh fk ghkldfs gjfodgdfsaklgnfjldgsdjklg fhaklsg hkdfl gdjlgsdjg dsg   ffhdhkfljdsaghfads ds bfdjklsafgdjks  dasf'
+    }
+  },
+  template: '<Vote/>'
+}
+</script>
+
+<style scoped>
+</style>
